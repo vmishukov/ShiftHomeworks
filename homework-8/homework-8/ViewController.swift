8cc3b6f8a144ce13fccecc74440a500dd6a1b533
ketamine <vladg3@mail.ru>
2024-06-15 15:11:50 +0700

    Remove StoryBoard

---
+++ homework-8/homework-8/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  homework-8
+//
+//  Created by Vladislav Mishukov on 15.06.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
